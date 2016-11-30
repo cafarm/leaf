d7111e2946cf723be104f3444e4ba27c581e7b48
Mark Cafaro <mark.cafaro@gmail.com>
2016-11-30 10:37:48 -0800

    Started implementing nearest neighbor

--- leaf.m
+++ leaf.m
@@ -1,14 +1,27 @@
-%% Load dataset table
+%% Extract Histogram of Curvature over Scale feature
+
+% This section extracts the Histogram of Curvature over Scale (HoCS) feature from each image as described by Kumar et. 
+% al. in Leafsnap: A Computer Vision System for Automatic Plant Species Identification. The basic idea is to calculate
+% the curvature around the parameter of each leaf at a number of different scales, bin these curvatures into histograms 
+% for each scale, and then concatenate the resulting set of histograms into a feature vector for each leaf.
+%
+% This process takes a long time so we save a file every time we calculate a good number of feature vectors in order to
+% avoid losing everything if something crashes. The resulting files are combine into a single file in the next section.
+
 t = readtable('leafsnap-dataset-images.txt', 'ReadVariableNames', true, 'Delimiter', 'tab');
 
-%% Extract Histogram of Curvature over Scale feature
 if ~exist('derived', 'dir')
     mkdir('derived');
 end
 
-objectsPerFile = 100;
+% The number of scales to calculate the parameter curvature at
 nscales = 25;
+
+% The number of bins to use when creating a histogram of the curvatures for each scale
 nbins = 21;
+
+% The number of objects to save out to each file
+objectsPerFile = 100;
 
 for i = 1:ceil(height(t)/objectsPerFile)
     startIndex = (i - 1) * objectsPerFile + 1;
@@ -42,8 +55,12 @@ for i = 1:ceil(height(t)/objectsPerFile)
 end
 
 %% Concatenate labels and features into a single file
-if ~exist('tables', 'dir')
-    mkdir('tables');
+
+% This section takes the set of feature tables created by the previous section, removes any objects that failed to
+% compute a feature vector, and combines them all into a dataset suitable for machine learning.
+
+if ~exist('table', 'dir')
+    mkdir('table');
 end
     
 labelFiles = cellstr(ls('derived/labels*'));
@@ -69,4 +86,24 @@ end
 
 dataset.gnd = allLabels;
 dataset.fea = allFeatures;
-save('tables/complete-dataset', 'dataset');
+save('table/complete-dataset', 'dataset');
+
+disp(['Number of objects in dataset: ' num2str(size(dataset.fea, 1))]);
+
+%% Nearest neighbor
+file = load('table/complete-dataset');
+dataset = file.dataset;
+
+% Kumar et. al. found the histogram intersection distance to perform better than L1, L2, Bhattacharyya distance, and X2
+histogramIntersectionDistance = @(a,b)nscales - sum(min(a, b), 2);
+
+mdl = fitcknn(dataset.fea, dataset.gnd(:,1), 'Distance', histogramIntersectionDistance);
+
+% Separate out the field images
+i = strcmp('field', dataset.gnd(:,2));
+fieldOnly.gnd = dataset.gnd(i,:);
+fieldOnly.fea = dataset.fea(i,:);
+
+cvmdl = crossval(mdl, 'Leaveout', 'on');
+
+cvmdlloss = kfoldLoss(cvmdl);
